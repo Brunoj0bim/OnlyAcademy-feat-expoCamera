d3624d0c37c2f36a6316c8119996eafb66279f55
Brunoj0bim <bjpires@minha.fag.edu.br>
2024-05-22 21:42:10 -0300

    Initial commit

---
+++ OnlyAcademy-feat-expoCamera/App.tsx
@@ -0,0 +1,19 @@
+// App.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../OnlyAcademy/src/screens/Home/index'
+import CameraScreen from '../OnlyAcademy/src/screens/Home/Camera'
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Camera" component={CameraScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
